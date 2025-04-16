6f85e7bde8996784edcc817a8e6f24f884ca85d6
AndreaCatapano <a.catapano96@gmail.com>
2025-04-16 16:49:43 +0200

    feat: completed bonus with call to api in products and css for card product

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+const url = "https://jsonplaceholder.typicode.com/posts"
 const Products = () => {
 
 
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get(url).
+            then(res => setProducts(res.data)).catch(err => console.error(err))
+    }, [])
+
     return (
-        <div>PAGINA PRODOTTI</div>
+        <div className="products-container">
+            {products.map(product => (
+                <div key={product.id} className="product-card">
+                    <h1 className="product-title">{product.title}</h1>
+                    <p className="product-body">{product.body}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
