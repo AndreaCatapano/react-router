77a40bbf2347bcd55eb81c36a145297897f17b99
AndreaCatapano <a.catapano96@gmail.com>
2025-04-16 14:00:50 +0200

    feat: created 3 pages for router

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+const About = () => {
+
+
+    return (
+        <div>PAGINA ABOUT</div>
+    )
+}
---
+++ src/pages/Products.jsx
@@ -0,0 +1,7 @@
+const Products = () => {
+
+
+    return (
+        <div>PAGINA PRODOTTI</div>
+    )
+}
